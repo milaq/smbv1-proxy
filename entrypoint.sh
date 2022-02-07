9527f2d4a86d65095a94f956cbb6ac29aa091cb3
milaq <micha.laqua@gmail.com>
2022-02-07 15:25:00 +0100

    force local user and group for proxy share

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,12 @@ if [[ -z $PROXY_REMOTE_SMB_VERSION ]]; then
   PROXY_REMOTE_SMB_VERSION=default
 fi
 
+groupadd "$PROXY_LOCAL_USERS"
+useradd -d /nonexistent -M -s /usr/sbin/nologin -g "$PROXY_LOCAL_USERS" "${PROXY_LOCAL_USERS}_${PROXY_USER}"
+
 mkdir "$PROXY_DIR"
 chattr +i "$PROXY_DIR"
-mount -t cifs //$PROXY_SERVER/$PROXY_SHARENAME -o username="$PROXY_USER",domain="$PROXY_DOMAIN",password="$PROXY_PASSWORD",iocharset=utf8,vers="$PROXY_REMOTE_SMB_VERSION" $PROXY_DIR
+mount -t cifs //$PROXY_SERVER/$PROXY_SHARENAME -o username="$PROXY_USER",domain="$PROXY_DOMAIN",password="$PROXY_PASSWORD",uid="${PROXY_LOCAL_USERS}_${PROXY_USER}",gid="${PROXY_LOCAL_USERS}",forceuid,forcegid,file_mode=0644,dir_mode=0755,iocharset=utf8,vers="$PROXY_REMOTE_SMB_VERSION" $PROXY_DIR
 if [[ $? -ne 0 ]]; then
   echo "Error: Mounting remote share failed"
   exit 1
@@ -18,9 +21,7 @@ fi
 rm /etc/samba/smb.conf
 cp /etc/samba/smb.conf.preset /etc/samba/smb.conf
 echo -e "[$PROXY_SHARENAME]\npath = $PROXY_DIR\nwriteable = yes\nbrowseable = yes\n" >> /etc/samba/smb.conf
-
-groupadd "$PROXY_LOCAL_USERS"
-useradd -d /nonexistent -M -s /usr/sbin/nologin -g "$PROXY_LOCAL_USERS" "${PROXY_LOCAL_USERS}_${PROXY_USER}"
 (echo "$PROXY_PASSWORD"; echo "$PROXY_PASSWORD") | smbpasswd -s -a "${PROXY_LOCAL_USERS}_${PROXY_USER}"
+
 
 /usr/sbin/smbd -F -S
